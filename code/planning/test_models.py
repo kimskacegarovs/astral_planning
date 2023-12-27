85682b8aed75dc8e560a77db37db29ab47770de1
kimskacegarovs <kimskacegarovs@gmail.com>
2023-12-27 10:13:06 +0200

    tests

--- code/planning/test_models.py
+++ code/planning/test_models.py
@@ -1,11 +1,21 @@
 import json
 import pytest
-from .models import (
-    Location,
-    Shipment,
-    Transport,
-    Route,
-)
+from .models import Location, Shipment, Transport, Route, Planning, LocationSearchResultData
+
+
+@pytest.fixture
+def transport():
+    return Transport.objects.create(name="Test Transport")
+
+
+@pytest.fixture
+def shipment():
+    return Shipment.objects.create(name="Test Shipment")
+
+
+@pytest.fixture
+def route():
+    return Route.objects.create(polyline=json.dumps([[1.0, 2.0], [3.0, 4.0]]))
 
 
 @pytest.mark.django_db
@@ -28,36 +38,42 @@ class TestCoordinatesMixin:
         with pytest.raises(ValueError):
             location.validate_coordinates()
 
-    # @pytest.mark.django_db
-    # class TestLocationSearchResultData:
-    #     def test_as_json(self):
-    #         search_result = LocationSearchResultData(search_text="Test", display_name="Test", coordinates="50.0,30.0")
-    #         search_result.save()
-    #         expected_json = f'{"id": "{str(search_result.id)}", "search_text": "Test", "display_name": "Test", "coordinates": "50.0,30.0"}'
-    #         assert search_result.as_json == expected_json
+    @pytest.mark.django_db
+    class TestLocationSearchResultData:
+        def test_as_json(self):
+            search_result = LocationSearchResultData(search_text="Test", display_name="Test", coordinates="50.0,30.0")
+            search_result.save()
 
-    # @pytest.mark.django_db
-    # class TestTransport:
-    #     def test_assign_shipment(self):
-    #         transport = Transport(name="Test Transport")
-    #         shipment = Shipment(name="Test Shipment")
-    #         planning = transport.assign_shipment(shipment)
-    #         assert planning.transport == transport
-    #         assert planning.shipment == shipment
+            expected_json = str(
+                {
+                    "id": str(search_result.id),
+                    "search_text": "Test",
+                    "display_name": "Test",
+                    "coordinates": "50.0,30.0",
+                }
+            ).replace("'", '"')
 
-    def test_unassign_shipment(self):
-        transport = Transport(name="Test Transport")
-        shipment = Shipment(name="Test Shipment")
+            assert search_result.as_json == expected_json
+
+
+@pytest.mark.django_db
+class TestTransport:
+    def test_assign_shipment(self, transport, shipment):
+        planning = transport.assign_shipment(shipment)
+        assert planning.transport == transport
+        assert planning.shipment == shipment
+
+    def test_unassign_shipment(self, transport, shipment):
         transport.assign_shipment(shipment)
+        assert transport.plannings.count() == 1
+
         transport.unassign_shipment(shipment)
         assert transport.plannings.count() == 0
 
-    # def test_planned_shipment(self):
-    #     transport = Transport(name="Test Transport")
-    #     shipment = Shipment(name="Test Shipment")
-    #     transport.assign_shipment(shipment)
-    #     planned_shipment = transport.planned_shipment()
-    #     assert planned_shipment == shipment
+    def test_planned_shipment(self, transport, shipment):
+        transport.assign_shipment(shipment)
+        planned_shipment = transport.planned_shipment()
+        assert planned_shipment == shipment
 
 
 @pytest.mark.django_db
@@ -67,13 +83,10 @@ class TestRoute:
         assert route.polyline_array == [[1.0, 2.0], [3.0, 4.0]]
 
 
-# @pytest.mark.django_db
-# class TestPlanning:
-#     def test_planning_creation(self):
-#         shipment = Shipment(name="Test Shipment")
-#         transport = Transport(name="Test Transport")
-#         route = Route(polyline=json.dumps([[1.0, 2.0], [3.0, 4.0]]))
-#         planning = Planning.objects.create(shipment=shipment, transport=transport, route=route)
-#         assert planning.shipment == shipment
-#         assert planning.transport == transport
-#         assert planning.route == route
+@pytest.mark.django_db
+class TestPlanning:
+    def test_planning_creation(self, transport, shipment, route):
+        planning = Planning.objects.create(shipment=shipment, transport=transport, route=route)
+        assert planning.shipment == shipment
+        assert planning.transport == transport
+        assert planning.route == route
